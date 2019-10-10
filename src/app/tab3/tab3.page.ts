95ed103d6e76e5247ac6b284a790646f4ea67194
syntappz <doddsanater87@gmail.com>
2019-10-10 02:03:55 +0100

    added reg tester

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,16 +1,51 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  selector: "app-tab3",
+  templateUrl: "tab3.page.html",
+  styleUrls: ["tab3.page.scss"]
 })
 export class Tab3Page {
-symbols = ['\\', '/', '[', ']', '.', '+', '^', '-', '$', '*', '_', '?', '{', '}', '(', ')']
+  symbols: object = [
+    "\\",
+    "/",
+    "[",
+    "]",
+    ".",
+    "+",
+    "^",
+    "-",
+    "$",
+    "*",
+    "_",
+    "?",
+    "{",
+    "}",
+    "(",
+    ")"
+  ];
   constructor() {}
+  stringInput: string;
+  regInput: string;
+  flagInput: string;
 
   symbolClicked(symbol: string) {
-    console.log(symbol);
+    if(this.regInput === undefined) {
+      this.regInput = symbol
+    }else{
+    this.regInput = this.regInput + symbol;
+      
+    }
+  }
+
+  regTester(string: string, reg: string, flag: string) {
+    const test = new RegExp(reg, flag)
+    return string.match(test)
+  }
+
+  test() {
+  console.log(this.regTester(this.stringInput, this.regInput, this.flagInput))
+
   }
 
 }
