f96d614e70705fdbcf9bdff1acecb6bd18422c57
syntappz <doddsanater87@gmail.com>
2019-10-11 14:29:23 +0100

    before fix

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -28,24 +28,79 @@ export class Tab3Page {
   stringInput: string;
   regInput: string;
   flagInput: string;
+  match: any;
+  chars = [];
+  regexResults = [];
+  indexArray = [];
+  outputArray = document.getElementsByClassName("char");
 
   symbolClicked(symbol: string) {
-    if(this.regInput === undefined) {
-      this.regInput = symbol
-    }else{
-    this.regInput = this.regInput + symbol;
-      
+    if (this.regInput === undefined) {
+      this.regInput = symbol;
+    } else {
+      this.regInput = this.regInput + symbol;
     }
+    this.checkRegex();
   }
 
-  regTester(string: string, reg: string, flag: string) {
-    const test = new RegExp(reg, flag)
-    return string.match(test)
+  regexMatchTester(str: string, reg: string, flag: string) {
+    this.regexResults = [];
+    const test = new RegExp(reg, flag);
+    const isRegex = test.test(str)
+    if(flag !== undefined && flag.length > 0) {
+      console.log('flag reg')
+    }else{
+      if(str !== undefined && str.length > 0) {
+        this.regexResults.push({
+          result: str.match(test),
+          index: str.match(test).index
+        });
+       }
+    }
+  
+  
+    
+    
+     
+  
+  }
+   test() {
+    
+   }
+
+  stringTyped() {
+    this.chars = [...this.stringInput];
+   // console.log(this.regInput)
+    if(this.regInput !== undefined) {
+      this.checkRegex()
+    }
+    
   }
 
-  test() {
-  console.log(this.regTester(this.stringInput, this.regInput, this.flagInput))
-
+  checkRegex() {
+    
+   
+      this.indexArray = []
+      this.regexMatchTester(this.stringInput, this.regInput, this.flagInput);
+      console.log(this.regexResults)
+      this.regexResults.forEach((x: any) => {
+      const amount = x.result[0].length;
+      const start = x.index;
+      
+      this.indexArray.push(this.getIndexes(start, amount))
+      
+    });
+    
+    
   }
 
+  getIndexes(start: number, amount: number) {
+    console.log('index check run')
+    const arr = [];
+    for (let i = start; i < (start + amount); i++) {
+      //console.log(i)
+      arr.push(i);
+    }
+  //  console.log(arr)
+  }
 }
