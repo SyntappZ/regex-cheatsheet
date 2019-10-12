8bebc3810db2cfad2438e65a773193b6d1088fcd
syntappz <doddsanater87@gmail.com>
2019-10-12 19:54:52 +0100

    finished global matching

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,6 +1,5 @@
 import { Component } from "@angular/core";
 
-
 @Component({
   selector: "app-tab3",
   templateUrl: "tab3.page.html",
@@ -31,7 +30,8 @@ export class Tab3Page {
   flagInput: string;
   match: any;
   chars = [];
-  
+  matchedAmount = 0;
+
   indexArray = [];
   outputArray = document.getElementsByClassName("char");
 
@@ -41,28 +41,52 @@ export class Tab3Page {
     } else {
       this.regInput = this.regInput + symbol;
     }
-   this.checkRegex()
+    this.checkRegex();
   }
 
-  
-  test() {
+  test() {}
+
+  globalMatchTest(str: string, reg: string, flag: string) {
+    let matchedArray = [];
+    let beginAndEnd = []
+    let match: RegExpExecArray;
+    const test = new RegExp(reg, flag);
+
+      if(reg.length > 0) {
+        while ((match = test.exec(str)) !== null) {
+          beginAndEnd.push([match.index, match.index + match[0].length]);
+        }
+      }
+      beginAndEnd.forEach(x => {
+        for(let i = x[0]; i < x[1]; i++) {
+          matchedArray.push(i)
+        }
+      })
+
     
-  }
-  
-  globalMatchTest() {
-
+      this.regexMatched(matchedArray);
   }
 
   stringTyped() {
     this.chars = [...this.stringInput];
-    if(this.regInput !== undefined) {
-    this.checkRegex()
+    if (this.regInput !== undefined) {
+      this.checkRegex();
     }
   }
 
   checkRegex() {
+    let global = false
+    if(this.flagInput !== undefined) {
+      global = this.flagInput.split('').includes('g')
+    }
     
-    this.matchTest(this.stringInput, this.regInput, this.flagInput)
+    
+    if(global) {
+      this.globalMatchTest(this.stringInput, this.regInput, this.flagInput);
+    }else{
+      this.matchTest(this.stringInput, this.regInput, this.flagInput);
+    }
+   
   }
 
   matchTest(str: string, reg: string, flag: string) {
@@ -70,30 +94,27 @@ export class Tab3Page {
     const test = new RegExp(reg, flag);
     const match = str.match(test)[0].length;
     const position = str.match(test).index;
-    for(let i = position; i <  position + match; i++) {
-      matchedArray.push(i)
+    for (let i = position; i < position + match; i++) {
+      matchedArray.push(i);
     }
     this.regexMatched(matchedArray);
+  }
+
+ 
+  regexMatched(arr: any[]) {
+    this.matchedAmount = arr.length;
     
-  }
-
-  // this.outputArray[pos].style.backgroundColor = '#fff'
-  // this.outputArray[pos].style.color = '#000'
-  regexMatched(arr) {
-   
-    const charArray = new Array(this.stringInput.length).fill(0).map((x, i) => x = i)
+    const charArray = new Array(this.stringInput.length)
+      .fill(0)
+      .map((x, i) => (x = i));
     charArray.forEach(pos => {
-      if(arr.includes(pos)) {
-        this.outputArray[pos].style.backgroundColor = '#900C3F';
-        this.outputArray[pos].style.color = '#fff';
-      }else{
-        this.outputArray[pos].style.backgroundColor = '#fff'
-        this.outputArray[pos].style.color = '#000' 
+      if (arr.includes(pos) && this.regInput.length > 0) {
+        this.outputArray[pos].style.backgroundColor = "#900C3F";
+        this.outputArray[pos].style.color = "#fff";
+      } else {
+        this.outputArray[pos].style.backgroundColor = "#fff";
+        this.outputArray[pos].style.color = "#000";
       }
-    })
-      
-   
+    });
   }
-
-  
 }
