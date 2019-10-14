5199c36b50a2a3ea2a9159963ab8b7d21b922973
syntappz <doddsanater87@gmail.com>
2019-10-14 16:51:13 +0100

    need to fix not valid regex error.

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 
+
 @Component({
   selector: "app-tab3",
   templateUrl: "tab3.page.html",
@@ -24,27 +25,32 @@ export class Tab3Page {
     "(",
     ")"
   ];
-  constructor() {}
+  constructor() { }
   stringInput: string;
   regInput: string;
   flagInput: string;
   match: any;
-  chars = [];
-  matchedAmount = 0;
+  chars: object = [];
+  matchedAmount: number = 0;
+  cursorPosition: number;
 
   indexArray = [];
-  outputArray = document.getElementsByClassName("char");
+  outputArray = <HTMLElement><unknown>document.getElementsByClassName("char");
 
   symbolClicked(symbol: string) {
     if (this.regInput === undefined) {
       this.regInput = symbol;
     } else {
-      this.regInput = this.regInput + symbol;
+      let start = this.regInput.substring(0, this.cursorPosition);
+      let end = this.regInput.substring(this.cursorPosition, this.regInput.length);
+      this.regInput = start + symbol + end;
+
+      this.cursorPosition += 1
     }
     this.checkRegex();
   }
 
-  test() {}
+
 
   globalMatchTest(str: string, reg: string, flag: string) {
     let matchedArray = [];
@@ -52,61 +58,77 @@ export class Tab3Page {
     let match: RegExpExecArray;
     const test = new RegExp(reg, flag);
 
-      if(reg.length > 0) {
-        while ((match = test.exec(str)) !== null) {
-          beginAndEnd.push([match.index, match.index + match[0].length]);
-        }
+    if (reg.length > 0) {
+      while ((match = test.exec(str)) !== null) {
+        beginAndEnd.push([match.index, match.index + match[0].length]);
       }
-      beginAndEnd.forEach(x => {
-        for(let i = x[0]; i < x[1]; i++) {
-          matchedArray.push(i)
-        }
-      })
+    }
+    beginAndEnd.forEach(x => {
+      for (let i = x[0]; i < x[1]; i++) {
+        matchedArray.push(i)
+      }
+    })
 
-    
-      this.regexMatched(matchedArray);
+    this.matchedAmount = beginAndEnd.length;
+    this.regexMatched(matchedArray);
   }
 
   stringTyped() {
-    this.chars = [...this.stringInput];
+    this.chars = this.stringInput.split('');
     if (this.regInput !== undefined) {
       this.checkRegex();
     }
   }
+  getCusorPostion(e) {
+    console.log(e.key)
+    if(e.key === undefined) {
+    this.cursorPosition = e.target.selectionStart;
+    }else{
+    this.cursorPosition = e.target.selectionStart;
+    console.log(this.cursorPosition)
+    }
+  }
 
   checkRegex() {
-    let global = false
-    if(this.flagInput !== undefined) {
+    let global: boolean = false
+
+    if (this.flagInput !== undefined) {
       global = this.flagInput.split('').includes('g')
     }
-    
-    
-    if(global) {
+    if (global && this.stringInput.length > 0) {
       this.globalMatchTest(this.stringInput, this.regInput, this.flagInput);
-    }else{
+    } else {
       this.matchTest(this.stringInput, this.regInput, this.flagInput);
     }
-   
+
   }
 
   matchTest(str: string, reg: string, flag: string) {
     const matchedArray = [];
-    const test = new RegExp(reg, flag);
-    const match = str.match(test)[0].length;
-    const position = str.match(test).index;
-    for (let i = position; i < position + match; i++) {
-      matchedArray.push(i);
+    let test: RegExp = new RegExp(reg, flag);
+    
+
+    
+
+    if (test.test(str)) {
+      const match: number = str.match(test)[0].length;
+      const position: number = str.match(test).index;
+      for (let i = position; i < position + match; i++) {
+        matchedArray.push(i);
+      }
     }
+    this.matchedAmount = matchedArray.length;
     this.regexMatched(matchedArray);
+
   }
 
- 
+
   regexMatched(arr: any[]) {
-    this.matchedAmount = arr.length;
-    
-    const charArray = new Array(this.stringInput.length)
+    if(this.stringInput !== undefined && this.stringInput.length > 0) {
+      const charArray = new Array(this.stringInput.length)
       .fill(0)
       .map((x, i) => (x = i));
+    
     charArray.forEach(pos => {
       if (arr.includes(pos) && this.regInput.length > 0) {
         this.outputArray[pos].style.backgroundColor = "#900C3F";
@@ -116,5 +138,6 @@ export class Tab3Page {
         this.outputArray[pos].style.color = "#000";
       }
     });
+  }
   }
 }
