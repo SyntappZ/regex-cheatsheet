b1eea09e7016862050cae7e1e652dea7f3be2214
syntappz <doddsanater87@gmail.com>
2019-10-24 15:58:56 +0100

    fixed switching between match and replace problems

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -16,9 +16,9 @@ export class Tab3Page {
   regexType: string = "match";
   onReplace: boolean = false;
   onMatch: boolean = true;
+  onFlag: boolean = false;
   indexArray = [];
   cursorPosition: number = 0;
-  //outputArray  = <HTMLElement>(<unknown>document.getElementsByClassName("char"));
   outputArray = document.getElementsByClassName("char");
 
   symbols: object = [
@@ -68,6 +68,90 @@ export class Tab3Page {
     this.checkRegex();
   }
 
+  flagTyped() {
+    setTimeout(() => {
+      this.checkRegex();
+    }, 20);
+  }
+
+  stringTyped() {
+    this.chars = this.stringInput.split("");
+    this.checkRegex();
+  }
+
+  segmentChanged(e) {
+    if (e.detail.value == "match") {
+      this.onMatch = true;
+      this.onReplace = false;
+      this.stringTyped();
+    } else {
+      this.onMatch = false;
+      this.onReplace = true;
+      let eArr = this.outputArray[Symbol.iterator]();
+      if (this.replaceWith === undefined) {
+        this.replaceWith = "";
+      }
+      setTimeout(() => {
+        for (let elem of eArr) {
+          elem.style.backgroundColor = "#fff";
+          elem.style.color = "#000";
+        }
+      }, 20);
+    }
+    this.checkRegex();
+  }
+
+  checkRegex() {
+    if (this.onMatch) {
+      let global: boolean = false;
+
+      if (this.flagInput !== undefined) {
+        global = this.flagInput.split("").includes("g");
+      }
+
+      if (global && this.stringInput.length > 0) {
+        this.globalMatchTest(this.stringInput, this.regInput, this.flagInput);
+      } else {
+        this.matchTest(this.stringInput, this.regInput, this.flagInput);
+      }
+    } else {
+      this.replaceRegex(
+        this.stringInput,
+        this.regInput,
+        this.flagInput,
+        this.replaceWith
+      );
+    }
+  }
+
+  replaceRegex(str: string, reg: string, flag: string, replaceWith: string) {
+    let replacing: RegExp = new RegExp(reg, flag);
+    let rep = str.replace(replacing, replaceWith);
+    if (reg.length > 0) {
+      this.chars = rep.split("");
+    } else {
+      this.chars = str.split("");
+    }
+  }
+
+  matchTest(str: string, reg: string, flag: string) {
+    const matchedArray = [];
+    let bracketTest = /\[(?!.*\])/.test(reg);
+    if (!bracketTest) {
+      let test: RegExp = new RegExp(reg, flag);
+      if (test.test(str)) {
+        const match: number = str.match(test)[0].length;
+        const position: number = str.match(test).index;
+        for (let i = position; i < position + match; i++) {
+          matchedArray.push(i);
+        }
+      }
+    }
+
+    this.matchedAmount = matchedArray.length;
+    this.regexMatched(matchedArray);
+  }
+
   globalMatchTest(str: string, reg: string, flag: string) {
     let matchedArray = [];
     let beginAndEnd = [];
@@ -88,89 +172,14 @@ export class Tab3Page {
     });
 
     this.matchedAmount = beginAndEnd.length;
-
-    this.regexMatched(matchedArray);
-  }
-
-  stringTyped() {
-    this.chars = this.stringInput.split("");
-
-    this.checkRegex();
-  }
-
-  segmentChanged(e) {
-    this.regInput = "";
-    this.chars = [];
-    this.flagInput = "";
-    this.stringInput = "";
-    this.matchedAmount = 0;
-    this.replaceWith = "";
-    if (e.detail.value == "match") {
-      this.onMatch = true;
-      this.onReplace = false;
-    } else {
-      this.onMatch = false;
-      this.onReplace = true;
-    }
-  }
-
-  checkRegex() {
-    if (this.onMatch) {
-      let global: boolean = false;
-
-      if (this.flagInput !== undefined) {
-        global = this.flagInput.split("").includes("g");
-      }
-      if (global && this.stringInput.length > 0) {
-        this.globalMatchTest(this.stringInput, this.regInput, this.flagInput);
-      } else {
-        this.matchTest(this.stringInput, this.regInput, this.flagInput);
-      }
-    } else {
-      this.replaceRegex(
-        this.stringInput,
-        this.regInput,
-        this.flagInput,
-        this.replaceWith
-      );
-    }
-  }
-  test() {
-    console.log(this.outputArray);
-  }
-
-  replaceRegex(str: string, reg: string, flag: string, replaceWith: string) {
-    let replacing: RegExp = new RegExp(reg, flag);
-
-    let rep = str.replace(replacing, replaceWith);
-    if (reg.length > 0) {
-      this.chars = rep.split("");
-    } else {
-      this.chars = str.split("");
-    }
-  }
-  matchTest(str: string, reg: string, flag: string) {
-    const matchedArray = [];
-    let bracketTest = /\[(?!.*\])/.test(reg);
-    if (!bracketTest) {
-      let test: RegExp = new RegExp(reg, flag);
-      if (test.test(str)) {
-        const match: number = str.match(test)[0].length;
-        const position: number = str.match(test).index;
-        for (let i = position; i < position + match; i++) {
-          matchedArray.push(i);
-        }
-      }
-    }
-
-    this.matchedAmount = matchedArray.length;
+    setTimeout(() => {}, 50);
     this.regexMatched(matchedArray);
   }
 
   regexMatched(arr: any[]) {
+    let eArr = this.outputArray[Symbol.iterator]();
+    let i = 0;
     setTimeout(() => {
-      let eArr = this.outputArray[Symbol.iterator]();
-      let i = 0;
       for (let elem of eArr) {
         if (arr.includes(i)) {
           elem.style.backgroundColor = "#900C3F";
@@ -181,6 +190,6 @@ export class Tab3Page {
         }
         i++;
       }
-    }, 100);
+    }, 20);
   }
 }
